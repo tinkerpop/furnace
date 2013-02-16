0c36769cb9861783e6f5ed3592ede92a66629424
Nicholas A. Stuart <nicholasastuart@gmail.com>
2013-02-16 16:23:36 -0600

    Moved shortest path algorithms to their own package

---
+++ src/main/java/com/tinkerpop/furnace/algorithms/shortestpath/SingleSourceShortestPathAlgorithm.java
@@ -0,0 +1,33 @@
+package com.tinkerpop.furnace.algorithms.shortestpath;
+
+import java.util.Map;
+
+import com.tinkerpop.blueprints.Graph;
+import com.tinkerpop.blueprints.Vertex;
+
+/**
+ * @author Nicholas A. Stuart (nicholasastuart@gmail.com)
+ */
+public abstract class SingleSourceShortestPathAlgorithm {
+	private final Graph graph;
+
+	public SingleSourceShortestPathAlgorithm(final Graph graph) {
+		this.graph = graph;
+	}
+
+	/**
+	 * 
+	 * @param source
+	 *            Source node from which to find the shortest path from.
+	 * @param weightPropertyName
+	 *            Name of the property on each edge to use for weight. Cannot be {@code null} or empty.
+	 * @param weightedEdgeLabel
+	 *            Edge label to go out from the vertex. {@code null} or empty is allowed and will use any edge label to traverse.
+	 * @return {@code Map<Vertex, Long>} with key of {@code Vertex} in the graph and value of distance to {@code Vertex}.
+	 */
+	public abstract Map<Vertex, Long> compute(final Vertex source, final String weightPropertyName, final String... weightedEdgeLabels);
+
+	public final Graph getGraph() {
+		return graph;
+	}
+}
