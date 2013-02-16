666ecfa1cd1fa42c5e0d71fd69be64758bed2de1
Nicholas A. Stuart <nicholasastuart@gmail.com>
2013-02-16 14:05:18 -0600

    Code Cleanup

--- src/main/java/com/tinkerpop/furnace/algorithms/ShortestPath.java
+++ src/main/java/com/tinkerpop/furnace/algorithms/ShortestPath.java
@@ -1,24 +1,28 @@
 package com.tinkerpop.furnace.algorithms;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import com.tinkerpop.blueprints.Element;
 import com.tinkerpop.blueprints.Graph;
 import com.tinkerpop.blueprints.Vertex;
-
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  * @author Marko A. Rodriguez (http://markorodriguez.com)
  */
 public class ShortestPath {
 
-    private final Graph graph;
+	private final Graph graph;
 
-    public ShortestPath(final Graph graph) {
-        this.graph = graph;
-    }
+	public ShortestPath(final Graph graph) {
+		this.graph = graph;
+	}
 
-    public List<Element> compute(final Vertex start, final Vertex end, final String... labels) {
-        return new ArrayList<Element>();
-    }
+	public List<Element> compute(final Vertex start, final Vertex end, final String... labels) {
+		return new ArrayList<Element>();
+	}
+
+	public Graph getGraph() {
+		return graph;
+	}
 }
