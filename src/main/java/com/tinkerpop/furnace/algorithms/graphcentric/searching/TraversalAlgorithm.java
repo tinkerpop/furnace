6e6002317ccc058c3025f9d29e3ef1c80503a246
Marko A. Rodriguez <okrammarko@gmail.com>
2013-08-16 11:39:34 -0600

    moved all the graphcentric algorithms over to graphcentric branch and not in master. Needs work to figure out the API for graphcentric work.

--- src/main/java/com/tinkerpop/furnace/algorithms/graphcentric/searching/TraversalAlgorithm.java
+++
@@ -1,9 +0,0 @@
-package com.tinkerpop.furnace.algorithms.graphcentric.searching;
-
-import java.util.List;
-
-import com.tinkerpop.blueprints.Vertex;
-
-public interface TraversalAlgorithm {
-	public List<Vertex> traverseTree(Vertex start);
-}
